ec30f151ae5f593d58373af32c0d3ceeeeb7b409
Kareem <kareem@doubleonemedia.com>
2012-03-15 22:52:40 -0400

    Removing unused functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,8 @@
 class ProjectsController < ApplicationController
   before_filter :authorize, :only => [:destroy, :edit, :show, :update]
 
-  def destroy
-    project = Project.find(params[:id])
-    project.destroy
-    redirect_to projects_url
-  end
-
-  def edit
-    @project = Project.find(params[:id])
-  end
-
   def index
     @projects = Project.active
-  end
-
-  def new
-    @project = Project.new
   end
 
   def show
@@ -28,13 +14,4 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def update
-    @project = Project.find(params[:id])
-
-    if @project.update_attributes params[:project]
-      redirect_to @project
-    else
-      render :action => :edit
-    end
-  end
 end
